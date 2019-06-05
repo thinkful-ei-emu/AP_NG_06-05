6dbb6b16c6d4d932704b9f1501bfd344d2685eee
Arpan Patel <adp8ke@gmail.com>
2019-06-05 11:57:39 -0400

    added update object drill

--- ObjectCreator.js
+++ ObjectCreator.js
@@ -8,3 +8,18 @@ function createMyObject() {
             }
         };
     }
+
+    //next drill
+
+    function updateObject(obj){
+        obj.foo = 'foo',
+        obj.bar = 'bar',
+        obj.bizz = 'bizz',
+        obj.bang = 'bang'
+
+        return obj;
+    }
+
+    //next drill
+
+    
